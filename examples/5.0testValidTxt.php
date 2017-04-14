ea02743b82c261ec225a93d4668f79d4eb717033
robmachado <linux.rlm@gmail.com>
2017-04-14 11:00:47 -0300

    [INC] ValidTXTTest

--- examples/5.0testValidTxt.php
+++ examples/5.0testValidTxt.php
@@ -8,6 +8,10 @@ use NFePHP\NFe\Common\ValidTXT;
 $txt = file_get_contents('fixtures/nfe_errado.txt');
 $errors = ValidTXT::isValid($txt);
 
+print_r($errors);
+
+/*
 foreach($errors as $error) {
     echo $error.'<br>';
 }
+*/
