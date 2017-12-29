636ffb42272d86073e3929d0e2937f15b1f8f451
robmachado <linux.rlm@gmail.com>
2017-12-29 08:36:26 -0200

    [UPD] correções em W02 tagICMSTot

--- src/Factories/Parser.php
+++ src/Factories/Parser.php
@@ -1555,7 +1555,7 @@ class Parser
      * NOTE: adjusted for NT2016_002_v1.30
      * W02|vBC|vICMS|vICMSDeson|vFCP|vBCST|vST|vFCPST|vFCPSTRet|vProd|vFrete|vSeg|vDesc|vII|vIPI|vIPIDevol|vPIS|vCOFINS|vOutro|vNF|vTotTrib|
      * NOTE: adjusted for NT2016_002_v1.31
-     * W02|vBC|vICMS|vICMSDeson|vFCP|vFCPUFDest|vICMSUFDest|vICMSUFRemet|vBCST|vST|vFCPST|vFCPSTRet|vProd|vFrete|vSeg|vDesc|vII|vIPI|vIPIDevol|vPIS|vCOFINS|vOutro|vNF|vTotTrib|
+     * W02|vBC|vICMS|vICMSDeson|vFCP|vBCST|vST|vFCPST|vFCPSTRet|vProd|vFrete|vSeg|vDesc|vII|vIPI|vIPIDevol|vPIS|vCOFINS|vOutro|vNF|vTotTrib|vFCPUFDest|vICMSUFDest|vICMSUFRemet|
      * @param stdClass $std
      * @return void
      */
