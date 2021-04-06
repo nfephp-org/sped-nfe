173205d7b3af247648d3b46a67f2b432f735fc0d
robmachado <linux.rlm@gmail.com>
2021-04-06 17:07:12 -0300

    ajustes qdo resposta da SEFAZ em branco

--- src/Common/Standardize.php
+++ src/Common/Standardize.php
@@ -105,6 +105,14 @@ class Standardize
                 return $key;
             }
         }
+        $result = $dom->getElementsByTagName('nfeResultMsg')->item(0);
+        if (!empty($result)) {
+            $cont = $result->textContent;
+            if (empty($cont)) {
+                throw new Exception('O retorno da SEFAZ veio em BRANCO, '
+                    . 'ou seja devido a um erro ou instabilidade na própria SEFAZ.');
+            }
+        }
         //documento does not belong to the SPED-NFe project
         throw DocumentsException::wrongDocument(7);
     }
