4af7a6491fe06f8713b660911c4e91066f2aea27
robmachado <linux.rlm@gmail.com>
2021-11-04 14:23:05 -0300

    ajuste decimais qTemp

--- src/Make.php
+++ src/Make.php
@@ -2788,7 +2788,7 @@ class Make
         $this->dom->addChild(
             $comb,
             "qTemp",
-            $this->conditionalNumberFormatting($std->qTemp, 4),
+            $this->conditionalNumberFormatting($std->qTemp, 2),
             false,
             "$identificador [item $std->item] Quantidade de combustível faturada à temperatura ambiente."
         );
