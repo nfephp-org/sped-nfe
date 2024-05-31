37ff5fce5d4a0d34a5e7f915b23554724f9979ba
Gerson Felipe Schwinn <gerson@onehost.com.br>
2024-05-31 13:37:41 -0300

    Melhorando formatação

--- tests/ToolsTest.php
+++ tests/ToolsTest.php
@@ -225,13 +225,12 @@ class ToolsTest extends NFeTestCase
 
     public function test_sefazCCe(): void
     {
-        $retorno = $this->tools->sefazCCe(
-            '35220605730928000145550010000048661583302923',
-            'Descrição da correção',
-        1,
-        new \DateTime('2024-05-31T11:59:12-03:00'),
-            '12345'
-        );
+        $chave = '35220605730928000145550010000048661583302923';
+        $xCorrecao = 'Descrição da correção';
+        $nSeqEvento = 1;
+        $dhEvento = new \DateTime('2024-05-31T11:59:12-03:00');
+        $lote = '12345';
+        $retorno = $this->tools->sefazCCe($chave, $xCorrecao, $nSeqEvento, $dhEvento, $lote);
         //@todo Testar o $retorno
         $request = $this->tools->getRequest();
         $esperado = $this->getCleanXml(__DIR__ . '/fixtures/xml/exemplo_xml_request_cce_cnpj.xml');
