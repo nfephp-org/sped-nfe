30842b521b2b4e8d439cc569d1e467e184fdd078
Gustavo Santarosa <bolota_xd@hotmail.com>
2025-06-09 22:49:38 -0300

    Criado o helper que valida algumas informações conforme o helper utilizado no laravel do pacote do vlucas

--- src/Libs/Helpers.php
+++ src/Libs/Helpers.php
@@ -57,3 +57,42 @@ if (!function_exists('throwIf')) {
         }
     }
 }
+
+if (!function_exists('env')) {
+    /**
+     * Simulates the behavior of Laravel's env().
+     *
+     * @param string $key
+     * @param mixed|null $default
+     * @return mixed
+     */
+    function env(string $key, $default = null)
+    {
+        $value = getenv($key);
+
+        if ($value === false) {
+            return $default;
+        }
+
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+            case 'false':
+            case '(false)':
+                return false;
+            case 'empty':
+            case '(empty)':
+                return '';
+            case 'null':
+            case '(null)':
+                return null;
+        }
+
+        if (strlen($value) > 1 && $value[0] === '"' && $value[-1] === '"') {
+            return substr($value, 1, -1);
+        }
+
+        return $value;
+    }
+}
