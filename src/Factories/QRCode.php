1d8bc500b3b05e1a5cc89fc7433481360cf992ad
Guilherme Verardi <46457332+guibverardi@users.noreply.github.com>
2025-07-23 15:19:53 -0300

    Update QRCode.php
    Remoção da validação CSC para a versão 300 do QRCode

--- src/Factories/QRCode.php
+++ src/Factories/QRCode.php
@@ -50,18 +50,25 @@ class QRCode
         $versao = trim($versao);
         $urlqr = trim($urlqr);
         $urichave = trim($urichave);
-        if (empty($token)) {
-            throw DocumentsException::wrongDocument(9); //Falta o CSC no config.json
-        }
-        if (empty($idToken)) {
-            throw DocumentsException::wrongDocument(10); //Falta o CSCId no config.json
-        }
-        if (empty($urlqr)) {
-            throw DocumentsException::wrongDocument(11); //Falta a URL do serviço NfeConsultaQR
-        }
+
         if (empty($versao)) {
             $versao = '200';
         }
+
+        if ($versao < 300) {
+            if (empty($token)) {
+                throw DocumentsException::wrongDocument(9); //Falta o CSC no config.json
+            }
+
+            if (empty($idToken)) {
+                throw DocumentsException::wrongDocument(10); //Falta o CSCId no config.json
+            }
+        }
+
+        if (empty($urlqr)) {
+            throw DocumentsException::wrongDocument(11); //Falta a URL do serviço NfeConsultaQR
+        }
+
         $nfe = $dom->getElementsByTagName('NFe')->item(0);
         $infNFe = $dom->getElementsByTagName('infNFe')->item(0);
         $layoutver = $infNFe->getAttribute('versao');
