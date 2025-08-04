8a0363cc10d815a7365018b8436b1c0f116d8211
João Gabriel <joao@suportec3l.com.br>
2025-08-04 14:53:16 -0400

    Update TraitTagDetIBSCBS.php
    Ajuste na geração da tag "vCredPresCondSus". A tag só pode ser informada para cCredPres com indicação de Condição Suspensiva. Como falta documentação de quando é suspensiva, será gerada quando o campo for declarado.

--- src/Traits/TraitTagDetIBSCBS.php
+++ src/Traits/TraitTagDetIBSCBS.php
@@ -458,13 +458,15 @@ trait TraitTagDetIBSCBS
             true,
             "$identificador Valor do Crédito Presumido (vCredPres)"
         );
-        $this->dom->addChild(
-            $gIBSCredPres,
-            "vCredPresCondSus",
-            $this->conditionalNumberFormatting($std->vCredPresCondSus),
-            true,
-            "$identificador Valor do Crédito Presumido em condição suspensiva. (vCredPres)"
-        );
+        if (!empty($std->vCredPresCondSus)) {
+            $this->dom->addChild(
+                $gIBSCredPres,
+                "vCredPresCondSus",
+                $this->conditionalNumberFormatting($std->vCredPresCondSus),
+                true,
+                "$identificador Valor do Crédito Presumido em condição suspensiva. (vCredPres)"
+            );
+        }
         $this->aIBSCredPres[$std->item] = $gIBSCredPres;
         return $gIBSCredPres;
     }
@@ -513,13 +515,15 @@ trait TraitTagDetIBSCBS
             true,
             "$identificador Valor do Crédito Presumido (vCredPres)"
         );
-        $this->dom->addChild(
-            $gCBSCredPres,
-            "vCredPresCondSus",
-            $this->conditionalNumberFormatting($std->vCredPresCondSus),
-            true,
-            "$identificador Valor do Crédito Presumido em condição suspensiva. (vCredPres)"
-        );
+        if (!empty($std->vCredPresCondSus)) {
+            $this->dom->addChild(
+                $gCBSCredPres,
+                "vCredPresCondSus",
+                $this->conditionalNumberFormatting($std->vCredPresCondSus),
+                true,
+                "$identificador Valor do Crédito Presumido em condição suspensiva. (vCredPres)"
+            );
+        }
         $this->aCBSCredPres[$std->item] = $gCBSCredPres;
         return $gCBSCredPres;
     }
