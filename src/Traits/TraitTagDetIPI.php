e92dc0cac1c750528183a6e54b474c1a8ea9c411
roberto <linux.rlm@gmail.com>
2025-08-25 17:12:45 -0300

    correção do IPI, pIPI pode ser ZERO

--- src/Traits/TraitTagDetIPI.php
+++ src/Traits/TraitTagDetIPI.php
@@ -81,7 +81,7 @@ trait TraitTagDetIPI
                 true,
                 "$identificador Código da situação tributária do IPI (CST)"
             );
-            if (!empty($std->vBC) && !empty($std->pIPI)) {
+            if (!empty($std->vBC) && isset($std->pIPI)) {
                 $this->dom->addChild(
                     $ipiTrib,
                     "vBC",
