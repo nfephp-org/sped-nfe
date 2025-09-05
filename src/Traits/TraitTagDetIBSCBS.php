2ffd1c7dd6526132eed47c2e6c52f5037add0f93
Rafael <rafael@sygmasistemas.com.br>
2025-09-05 10:33:12 -0300

    Correção das tags do grupo gTribCompraGov

--- src/Traits/TraitTagDetIBSCBS.php
+++ src/Traits/TraitTagDetIBSCBS.php
@@ -559,44 +559,44 @@ trait TraitTagDetIBSCBS
         $this->dom->addChild(
             $gTrib,
             "pAliqIBSUF",
-            $this->conditionalNumberFormatting($std->pIBSUF, 4),
+            $this->conditionalNumberFormatting($std->pAliqIBSUF, 4),
             true,
-            "$identificador Alíquota do IBS de competência do Estado. (pIBSUF)"
+            "$identificador Alíquota do IBS de competência do Estado. (pAliqIBSUF)"
         );
         $this->dom->addChild(
             $gTrib,
-            "vIBSUF",
-            $this->conditionalNumberFormatting($std->pIBSUF),
+            "vTribIBSUF",
+            $this->conditionalNumberFormatting($std->vTribIBSUF),
             true,
-            "$identificador Valor do Tributo do IBS da UF calculado. (vIBSUF)"
+            "$identificador Valor do Tributo do IBS da UF calculado. (vTribIBSUF)"
         );
         $this->dom->addChild(
             $gTrib,
-            "pIBSMun",
-            $this->conditionalNumberFormatting($std->pIBSMun, 4),
+            "pAliqIBSMun",
+            $this->conditionalNumberFormatting($std->pAliqIBSMun, 4),
             true,
-            "$identificador Alíquota do IBS de competência do Município. (pIBSMun)"
+            "$identificador Alíquota do IBS de competência do Município. (pAliqIBSMun)"
         );
         $this->dom->addChild(
             $gTrib,
-            "vIBSMun",
-            $this->conditionalNumberFormatting($std->vIBSMun),
+            "vTribIBSMun",
+            $this->conditionalNumberFormatting($std->vTribIBSMun),
             true,
-            "$identificador Valor do Tributo do IBS do Município calculado. (vIBSMun)"
+            "$identificador Valor do Tributo do IBS do Município calculado. (vTribIBSMun)"
         );
         $this->dom->addChild(
             $gTrib,
-            "pCBS",
-            $this->conditionalNumberFormatting($std->pCBS, 4),
+            "pAliqCBS",
+            $this->conditionalNumberFormatting($std->pAliqCBS, 4),
             true,
-            "$identificador Alíquota da CBS. (pCBS)"
+            "$identificador Alíquota da CBS. (pAliqCBS)"
         );
         $this->dom->addChild(
             $gTrib,
-            "vCBS",
-            $this->conditionalNumberFormatting($std->vCBS),
+            "vTribCBS",
+            $this->conditionalNumberFormatting($std->vTribCBS),
             true,
-            "$identificador Valor do Tributo da CBS calculado. (vCBS)"
+            "$identificador Valor do Tributo da CBS calculado. (vTribCBS)"
         );
         $this->aGTribCompraGov[$std->item] = $gTrib;
         return $gTrib;
