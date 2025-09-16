47a7bc45e27e38851c6bc51d8395366a3b7c4b95
roberto <linux.rlm@gmail.com>
2025-09-16 16:41:44 -0300

    ajustes MakeDevTest e TraitTagIde

--- src/Traits/TraitTagIde.php
+++ src/Traits/TraitTagIde.php
@@ -81,11 +81,15 @@ trait TraitTagIde
             $std->cDV = 0;
         }
         if (empty($std->dhEmi)) {
-            try {
-                $tz = TimeZoneByUF::get($std->cUF);
-                $std->dhEmi = (new DateTime('now', new DateTimeZone($tz)))->format('Y-m-d\TH:i:sP');
-            } catch (Exception $e) {
+            if (empty($std->cUF) || !in_array($std->cUF, [
+                    12, 27, 13, 16, 29, 23, 53, 32, 52, 21, 31, 50, 51,
+                    15, 25, 26, 22, 41, 33, 24, 11, 14, 43, 42, 28, 35, 17
+                ])
+            ) {
                 $this->errors[] = "$identificador Campo cUF incorreto !";
+            } else {
+                $tz = TimeZoneByUF::get($std->cuf);
+                $std->dhEmi = (new DateTime('now', new DateTimeZone($tz)))->format('Y-m-d\TH:i:sP');
             }
         }
         if (!empty($std->dhSaiEnt) && !empty($std->dhEmi)) {
@@ -118,7 +122,7 @@ trait TraitTagIde
             }
         }
         $this->tpAmb = $std->tpAmb;
-        $this->mod = $std->mod;
+        $this->mod = empty($std->mod) ? '55' : $std->mod;
 
         $ide = $this->dom->createElement("ide");
         $this->dom->addChild(
