e1ceedff0e50278aec3ea01b7b4845613ada002c
Maykon Dias <62573242+Maykon-JDS@users.noreply.github.com>
2025-10-02 18:55:01 +0000

    ajusta a função sefazApropriacaoCreditoComb
    - ajusta o nItem de elemento para atributo
    - correção das tags qComb e uComb

--- src/Traits/TraitEventsRTC.php
+++ src/Traits/TraitEventsRTC.php
@@ -339,13 +339,12 @@ trait TraitEventsRTC
             $vi = number_format($item->vIBS, 2, '.', '');
             $vc = number_format($item->vCBS, 2, '.', '');
             $qtd = number_format($item->quantidade, 4, '.', '');
-            $gc = "<gConsumoComb>"
-                . "<nItem>{$item->item}</nItem>"
+            $gc = "<gConsumoComb nItem=\"{$item->item}\">"
                 . "<vIBS>{$vi}</vIBS>"
                 . "<vCBS>{$vc}</vCBS>"
                 . "<gControleEstoque>"
-                . "<qConsumo>{$qtd}</qConsumo>"
-                . "<uConsumo>{$item->unidade}</uConsumo>"
+                . "<qComb>{$qtd}</qComb>"
+                . "<uComb>{$item->unidade}</uComb>"
                 . "</gControleEstoque>"
                 . "</gConsumoComb>";
             $tagAdic .= $gc;
